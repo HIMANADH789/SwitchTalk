dfc70e4b55dadf136456cd903f0216f21402b83d
HIMANADH789 <himanadhkondabathini@gmail.com>
2025-07-04 10:20:09 +0530

    Updations

--- backend/app.js
+++ backend/app.js
@@ -92,7 +92,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+    console.log('Server is running on port ${PORT}');
 });
 
 process.on('SIGTERM', () => {
